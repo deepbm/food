cc8883cf9aa0163ebb1c0517c97ff8a643ba5a55
양희연 <deepbm27@gmail.com>
2023-07-19 22:47:46 +0900

    Add: pagination 😂

--- src/api/foods.js
+++ src/api/foods.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-export async function getFoods(sort = 'createdAt') {
-  const { data } = await axios.get(`https://learn.codeit.kr/3507/foods?order=${sort}`);
+export async function getFoods(sort = 'createdAt', limit = 10, { pageParam }) {
+  let fetchUrl = `https://learn.codeit.kr/api/foods?order=${sort}&limit=${limit}`;
+  if (pageParam) {
+    fetchUrl = `https://learn.codeit.kr/api/foods?cursor=${pageParam}&limit=${limit}`;
+  }
+  const { data } = await axios.get(fetchUrl);
   return data;
 }
--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import styles from './Foods.module.css';
 import { getFoods } from '../api/foods';
 import FoodItem from './FoodItem';
-import { useQuery } from '@tanstack/react-query';
+
+const LIMIT = 10;
 
 export default function Foods() {
   const [sort, setSort] = useState('createdAt');
@@ -10,32 +12,35 @@ export default function Foods() {
     // setFoods(prev => prev.filter(food => food.id !== id));
   };
 
-  const {
-    isLoading,
-    error,
-    data: foods,
-  } = useQuery({
-    queryKey: ['foods', sort],
-    queryFn: async () => {
-      return getFoods(sort) //
-        .then(data => {
-          const { foods } = data;
-          return foods;
-        });
-    },
-    staleTime: 1000 * 60 * 5,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: ['foods', sort],
+      queryFn: async ({ pageParam = '' }) => getFoods(sort, LIMIT, { pageParam }),
+      getNextPageParam: lastPage => lastPage.paging.nextCursor,
+      select: data => ({
+        pages: data?.pages.flatMap(page => page.foods),
+        pageParams: data.pageParams,
+      }),
+    });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>데이터를 불러오지 못했습니다. 잠시후 다시 시도해주세요.</p>;
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'error') return <p>데이터를 불러오지 못했습니다. 잠시후 다시 시도해주세요.</p>;
 
   return (
     <>
       <button onClick={() => setSort('createdAt')}>최신순</button>
       <button onClick={() => setSort('calorie')}>칼로리순</button>
       <ul className={styles.foods}>
-        {foods && foods.map(food => <FoodItem key={food.id} food={food} onDelete={handleDelete} />)}
+        {data.pages.map(food => (
+          <FoodItem key={food.id} food={food} onDelete={handleDelete} />
+        ))}
       </ul>
+      {hasNextPage && (
+        <button onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+          더보기
+        </button>
+      )}
+      <p>{isFetching && !isFetchingNextPage ? '불러오는중...' : null}</p>
     </>
   );
 }
