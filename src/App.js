10c83a370f66116221957f0f91e9c6640261b45f
양희연 <deepbm27@gmail.com>
2023-07-19 21:21:29 +0900

    Update: show food list with useQuery function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Foods from './components/Foods';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider QueryClientProvider client={queryClient}>
       <Foods />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Foods.module.css';
 import { getFoods } from '../api/foods';
 import FoodItem from './FoodItem';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Foods() {
-  const [foods, setFoods] = useState();
   const [sort, setSort] = useState('createdAt');
   const handleDelete = id => {
-    setFoods(prev => prev.filter(food => food.id !== id));
+    // setFoods(prev => prev.filter(food => food.id !== id));
   };
 
-  useEffect(() => {
-    getFoods(sort) //
-      .then(data => {
-        const { foods } = data;
-        return setFoods(foods);
-      });
-  }, [sort]);
+  const {
+    isLoading,
+    error,
+    data: foods,
+  } = useQuery({
+    queryKey: ['foods', sort],
+    queryFn: async () => {
+      return getFoods(sort) //
+        .then(data => {
+          const { foods } = data;
+          return foods;
+        });
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>데이터를 불러오지 못했습니다. 잠시후 다시 시도해주세요.</p>;
 
   return (
     <>
